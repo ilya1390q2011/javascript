c233eeac15c1090aebd124dc8f7937a39bc4d1ab
Amirkakaie <amir.kakaie.vr@gmail.com>
2025-09-06 13:04:02 +0330

    jaavaa

---
+++ js/app.js
@@ -0,0 +1,23 @@
+alert("hello Eliya.")
+
+confirm("do you want to exit?")
+
+prompt("whats your favorite search")
+
+
+
+
+
+
+
+
+
+
+let user ="eliya ghasemi"
+let age  =25;
+let isMarried 
+
+
+
+
+
